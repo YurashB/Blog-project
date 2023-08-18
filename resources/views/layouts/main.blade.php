5f8209d4ac98f73725e5872f81f04632550f93b7
Bohdan Yurash <yurash.bogdan@gmail.com>
2023-08-18 23:58:10 +0300

    add bootstrap and change main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,19 +5,25 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Document</title>
 </head>
 <body>
-<div>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="{{route('main.index')}}">Main</a></li>
-                <li><a href="{{route('posts.index')}}">Posts</a></li>
-                <li><a href="{{route('about.index')}}">About</a></li>
-                <li><a href="{{route('contacts.index')}}">Contacts</a></li>
-            </ul>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="{{route('main.index')}}">Main</a>
+                        <a class="nav-link" href="{{route('posts.index')}}">Posts</a>
+                        <a class="nav-link" href="{{route('about.index')}}">About</a>
+                        <a class="nav-link" href="{{route('contacts.index')}}">Contacts</a>
+                    </div>
+                </div>
+            </div>
         </nav>
+
     </div>
     @yield('content')
 </div>
