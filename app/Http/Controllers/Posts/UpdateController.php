b46a8ecf1f72d3583325ad6ebb7417338398378d
Bohdan Yurash <yurash.bogdan@gmail.com>
2023-08-21 15:48:47 +0300

    add db seed to generate data

--- app/Http/Controllers/Posts/UpdateController.php
+++ app/Http/Controllers/Posts/UpdateController.php
@@ -6,17 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\UpdateRequest;
 use App\Models\Post;
 
-class UpdateController extends Controller
+class UpdateController extends BaseController
 {
 
     public function __invoke(UpdateRequest $request, Post $post)
     {
         $data = $request->validated();
-        $tags = $data['tags_id'];
-        unset($data['tags_id']);
 
-        $post->update($data);
-        $post->tags()->sync($tags);
+        $this->service->update($post, $data);
+
         return redirect()->route('post.show', $post->id);
     }
 }
