794e2fbdf9838bacfd93ea76e72a46b9d72c115a
Bohdan Yurash <yurash.bogdan@gmail.com>
2023-08-21 16:00:45 +0300

    add pagination

--- app/Http/Controllers/Posts/IndexController.php
+++ app/Http/Controllers/Posts/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends Controller
 
     public function __invoke()
     {
-        $posts = Post::all();
+        $posts = Post::query()->paginate(10);
         return view('post.index', compact('posts'));
     }
 }
