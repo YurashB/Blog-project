e7986417a01a33beeb8fa12005b7b34d99f9d03b
Bohdan Yurash <yurash.bogdan@gmail.com>
2023-08-21 12:30:34 +0300

    add requests for post with validation rules

--- app/Http/Controllers/Posts/StoreController.php
+++ app/Http/Controllers/Posts/StoreController.php
@@ -3,20 +3,16 @@
 namespace App\Http\Controllers\Posts;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 use App\Models\Post;
+use Illuminate\Session\Store;
 
 class StoreController extends Controller
 {
 
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = \request()->validate([
-            'title' => 'required|string',
-            'content' => 'string',
-            'image' => 'string',
-            'category_id' => '',
-            'tags_id' => ''
-        ]);
+        $data = $request->validated();
         $tags = $data['tags_id'];
         unset($data['tags_id']);
 
